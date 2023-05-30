d54f4422e947912372e723fa808822277208e0f5
Mekael Wasti <mekael_wasti@hotmail.com>
2023-05-29 20:09:33 -0400

    video test

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,20 @@
 import '../App.css';
+import React, { useEffect, useRef } from 'react';
+import videoFile from '../assets/testVideo.mp4';
+
 
 function HomePage() {
+    const videoRef = useRef(null);
+
+  useEffect(() => {
+    videoRef.current.play();
+  }, []);
+
     return (
         <div className="home-page">
             <header className="App-header">
                 <p>SOURCE MATERIAL</p>
+                <video ref={videoRef} src={videoFile} controls autoplay loop muted></video>
                 <p>2023</p>
             </header>
         </div>
