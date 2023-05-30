ae93fc885c6873c808a02be2f796bee7694ad064
Mekael Wasti <mekael_wasti@hotmail.com>
2023-05-29 22:01:41 -0400

    test

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -14,7 +14,7 @@ function HomePage() {
         <div className="home-page">
             <header className="App-header">
                 <p>SOURCE MATERIAL</p>
-                <video ref={videoRef} src={videoFile} controls autoplay loop muted playsinLine></video>
+                <video ref={videoRef} src={videoFile} controls autoplay loop muted playsInline></video>
                 <p>2023</p>
             </header>
         </div>
